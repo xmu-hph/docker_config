8b4b644ff991c8d02939850747de691aa5d6182c
huph210950 <huph210950@gmail.com>
2024-04-26 18:16:20 +0800

    push

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,13 @@ FROM ph210950/traffic_signal_control:traffic_torch201_cuda118_cudnn8_ubuntu2204
 #RUN git clone git@github.com:cityflow-project/CityFlow.git
 #RUN cd CityFlow && pip install .
 #CMD ["/bin/bash"]
-RUN pip install argparse
+RUN apt install -y swig vim
+RUN pip install argparse optuna gym[box2d] mujoco mediapy
+RUN git clone https://github.com/google-deepmind/mujoco_menagerie.git
+#RUN pip install mujoco_py
+#RUN wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
+#RUN mkdir ~/.mujoco
+#RUN tar -zxvf mujoco210-linux-x86_64.tar.gz -C ~/.mujoco
+#RUN echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin' >> ~/.bashrc
 RUN ln -s /usr/bin/python3 /usr/bin/python
 
--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ docker build -t $imgname .
 #docker push $imgname
 #docker rmi -f $imgname
 docker run -itd --gpus all --privileged --net=host --pid=host --ipc=host -v /home/thu_huph22/mnt:/root/hupenghui --name=hupenghui-$imgtag $imgname /bin/bash
+#docker push harbor.4pd.io/lab-platform/pk_platform/model_services/physics_hph:traffic_torch201_cuda118_cudnn8_ubuntu2204
+
+docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v ollama:/root/.ollama -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://host.docker.internal:11434 --name=open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
+docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name hupenghui-open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
+docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://host.docker.internal:11434  --name open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
+# ok
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
