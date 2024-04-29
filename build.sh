62f42263784591be13a3d862265152683fee451e
huph210950 <huph210950@gmail.com>
2024-04-29 21:30:47 +0800

    new

--- build.sh
+++ build.sh
@@ -11,7 +11,12 @@ docker run -itd --gpus all --privileged --net=host --pid=host --ipc=host -v /hom
 
 docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v ollama:/root/.ollama -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://host.docker.internal:11434 --name=open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
 docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name hupenghui-open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
-docker run -d --gpus all -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://host.docker.internal:11434  --name open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
+docker run -d --gpus all -p 3001:8080 --add-host=host.docker.internal:host-gateway -v /home/thu_huph22/huph_open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11435  --name hph_open-webui --restart always ghcr.nju.edu.cn/open-webui/open-webui:main
 # ok
 docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+docker run -d --gpus all --privileged --net=host --pid=host --ipc=host -v ollama:/root/.ollama --name=hupenghui-ollama docker.nju.edu.cn/ollama/ollama:0.1.32
+
+
+docker run -itd --gpus all --privileged --net=host --pid=host --ipc=host -v /home/thu_huph22/mynodejs:/root/mnt --name=hupenghui-nodejs node:22.0-bullseye
 
