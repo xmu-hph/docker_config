301c0b9180bfc10c3d438b66f3fa2efd7749eca4
huph210950 <huph210950@gmail.com>
2024-05-02 13:01:22 +0800

    renew

--- for_self_study/build.sh
+++ for_self_study/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -Ceu
-imgroot="harbor.4pd.io/lab-platform/pk_platform/model_services/hph_for_self_study_base:"
-imgtag="traffic_torch201_cuda118_cudnn8_ubuntu2204_debug001"
+imgroot="ph210950/traffic_signal_control:"
+imgtag="traffic_torch201_cuda118_cudnn8_ubuntu2204_debug002"
 imgname=$imgroot$imgtag
 docker build -t $imgname .
 docker push $imgname
 #docker rmi -f $imgname
-docker run -itd --gpus all --privileged --net=host --pid=host --ipc=host -v /home/thu_huph22/mnt:/root/hupenghui --name=hupenghui-$imgtag $imgname /bin/bash
+docker run -itd --gpus all --privileged --net=host --pid=host --ipc=host -v /home/thu_huph22/mnt:/home/mnt --name=hupenghui-$imgtag $imgname /bin/bash
